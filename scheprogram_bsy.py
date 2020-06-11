cb84a33f371bae4d0da5b8572a6c31941786f1eb
baesy828 <sunybae828@gmail.com>
2020-06-11 13:43:06 +0900

    0611

--- scheprogram_bsy.py
+++ scheprogram_bsy.py
@@ -1,5 +1,6 @@
 from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 from flask import Flask, render_template, jsonify, request
+import csv
 app = Flask(__name__)
 
 # URL 별로 함수명이 같거나,
@@ -16,32 +17,40 @@ def home():
     return render_template('index.html')
 
 # 스케쥴 입력을 포스트로 db에 보내기
-@app.route('/schedules', methods=['POST', 'PUT'])
+@app.route('/schedules', methods=['POST'])
 def input_schedule():
-    if request.method == 'POST':
-        date_receive = request.form['date_give'].strip()
-        project_receive = request.form['project_give']
-        content_receive = request.form['content_give']
-        dodont_receive = request.form['dodont_give']
+    date_receive = request.form['date_give'].strip()
+    project_receive = request.form['project_give']
+    content_receive = request.form['content_give']
+    dodont_receive = request.form['dodont_give']
 
-        schedule = {
-            'scheduleDate': date_receive,
-            'scheduleProject': project_receive,
-            'scheduleContent': content_receive,
-            'scheduleDodont': dodont_receive
-        }
+    schedule = {
+        'scheduleDate': date_receive,
+        'scheduleProject': project_receive,
+        'scheduleContent': content_receive,
+        'scheduleDodont': dodont_receive
+    }
 
-        db.schedules.insert_one(schedule)
-        return jsonify({'result': 'success'})
+    db.schedules.insert_one(schedule)
+    return jsonify({'result': 'success'})
 
-    #else if request.method == 'PUT':
-    #    delschedule = 
+    #elif request.method == 'PUT':
+    #    delschedule = request.form
 
+@app.route('/delete_schedule', methods=['POST'])
+def delete_schedule():
+    project_receive = request.form['scheduleProject']
+    content_receive = request.form['scheduleContent']
+    db.schedules.update({'scheduleProject': project_receive, 'scheduleContent': content_receive},
+        {'$set': {
+            'scheduleDodont': 'N'
+        }})
+    return jsonify({'result': 'success'})
 
 # 스케쥴 입력을 겟으로 프론트에 보내기
 @app.route('/schedules', methods=['GET'])
 def show_schedule():
-    schedules = list(db.schedules.find({}, {'_id': 0}))
+    schedules = list(db.schedules.find({'scheduleDodont': 'Y'}, {'_id': 0}))
     return jsonify({'result': 'success', 'schedules': schedules})
 
 
@@ -61,10 +70,16 @@ def input_project():
     db.projects.insert_one(project)
     return jsonify({'result': 'success'})
 
+@app.route('/delete_project', methods=['POST'])
+def delete_project():
+    name_receive = request.form['name_give']
+    db.projects.update({'projectName': name_receive}, {'$set': {'isDeleted': 'Y'}})
+    return jsonify({'result': 'success'})
+
 # 프로젝트 입력을 겟으로 프론트에 보내기
 @app.route('/projects', methods=['GET'])
 def show_project():
-    projects = list(db.projects.find({}, {'_id': 0}))
+    projects = list(db.projects.find({'isDeleted': 'N'}, {'_id': 0}))
     return jsonify({'result': 'success', 'projects': projects})
 
 
@@ -76,14 +91,8 @@ def input_review():
     prjreview_receive = request.form['prjreview_give']
     schereview_receive = request.form['schereview_give']
 
-    review = {
-        'OX': OX_receive,
-        'review': review_receive,
-        'whatProject': prjreview_receive,
-        'whatSchedule': schereview_receive
-    }
-
-    db.reviews.insert_one(review)
+    db.schedules.update_one({'scheduleProject': prjreview_receive, 'scheduleContent': schereview_receive},
+        {'$set': {'scheduleReview': review_receive, 'scheduleOX': OX_receive}})
     return jsonify({'result': 'success'})
 
 
